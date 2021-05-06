3c05886a3319f48fabb65ccafbd88d72c3934bad
Jessica  Fong Ng <jessicafongng@Jessicas-MacBook-Air-2.local>
2021-05-06 17:27:11 -0400

    it runs?

--- src/main/scala/final_project/main.scala
+++ src/main/scala/final_project/main.scala
@@ -1,6 +1,5 @@
 package final_project
 
-
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.SparkContext
@@ -11,44 +10,12 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.log4j.{Level, Logger}
 
 object main{
-
-/* example undirected graph
- val edges: RDD[Edge[Int]] =
- sc.parallelize(Seq(Edge(1L, 2L,1), Edge(2L,3L,1), Edge(5L,3L,0), Edge(3L,4L,1),
-                    Edge(4L,5L,1), Edge(5L,1L,1), Edge(1L,3L,1), Edge(1L,4L,0)))
- val g_in = Graph.fromEdges[(Int, Long), Int](edges, (0, 0L))
-
-var k = result
-val v_in = result.vertices.filter({case (id, x) => (x._1 == 1)}).collect()
-for (x <- v_in){
-  k = k.mapEdges(
-   //id => if ((id.srcId == 1 && id.dstId == 2) || (id.dstId == 2 && id.srcId == 1)) (1) else (id.attr)
-   id => if ((id.srcId == x._2._2 && id.dstId == x._1) || (id.dstId == x._2._2 && id.srcId == x._1)) (1) else (id.attr)
-   )
-}
-
-
-k.edges.collect()
-
-*/
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
 
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-
-/*
-select a random neighbors
-
-  def select_neighbor(g_in: Graph[Int, Int], vertex: VertexId): VertexId = {
-    val r = scala.util.Random
-    val neighbors = g.triplets.collect{
-      case t if (t.srcId == vertex) => t.dstId
-      case l if (l.dstId == vertex) => l.srcId}.collect()
-    return(neighbors(r.nextInt(neighbors.size)))
-  }
-*/
 
   def Israli(g_in: Graph[(Int, Long), Int]): Graph[(Int, Long), Int] = {
     val r = scala.util.Random
@@ -59,22 +26,22 @@ select a random neighbors
     while (active_v > 1) {
     counter+=1;
       //decide to propose to who
-      var v_propose = g.aggregateMessages[(Int, Long, Float)]( //(status, to, random_value)
+      var v_propose = g.aggregateMessages[(Int, Long, Float, Int)]( //(status, to, random_value, random int)
           d => { // Map Function
             if (d.dstAttr._1 == 1 || d.srcAttr._1 == 1) { // vertex used
-              d.sendToDst((d.dstAttr._1, d.dstAttr._2, -1F));
-              d.sendToSrc((d.srcAttr._1, d.srcAttr._2, -1F));
+              d.sendToDst((d.dstAttr._1, d.dstAttr._2, -1F, -1));
+              d.sendToSrc((d.srcAttr._1, d.srcAttr._2, -1F, -1));
             } else {
-              d.sendToDst((d.dstAttr._1, d.srcId, r.nextFloat));
-              d.sendToSrc((d.srcAttr._1, d.dstId, r.nextFloat));
+              d.sendToDst((d.dstAttr._1, d.srcId, r.nextFloat, -1));
+              d.sendToSrc((d.srcAttr._1, d.dstId, r.nextFloat, -1));
             }
           },
-          (a,b) => (if (a._3 > b._3) a else (b))
+          (a,b) => (if (a._1 == 1) a else if (b._1 == 1) b else if (a._3 > b._3) a else b)
       )
       var g2 = Graph(v_propose, g.edges)
 
       //propose and accept
-      var v_p = g2.aggregateMessages[(Int, Long, Float, Int)]( //(status, from, value, 0or1)
+      v_propose = g2.aggregateMessages[(Int, Long, Float, Int)]( //(status, from, value, 0or1)
         d => {
           if (d.dstAttr._1 == 1 || d.srcAttr._1 == 1) { // vertex used
             d.sendToDst((d.dstAttr._1, d.dstAttr._2, -1F, -1));
@@ -84,12 +51,12 @@ select a random neighbors
             d.sendToSrc(if (d.dstAttr._2 == d.srcId) (d.srcAttr._1, d.dstId, r.nextFloat, r.nextInt(2)) else (d.srcAttr._1, -1L, 1.1F, r.nextInt(2)))
             }
         },
-        (a,b) => (if (a._3 > -1) {if ((a._3) < (b._3)) (b) else (a) } else b) //select
+        (a,b) => (if (a._1 == 1) a else if (b._1 == 1) b else if ((a._3) < (b._3)) (b) else (a) ) //select
       )
-      var g3= Graph(v_p, g.edges)
+      g2 = Graph(v_propose, g.edges)
 
       //fitler edges
-      var v_deactivate = g3.aggregateMessages[(Int, Long)]( //(status, otherVertex)
+      var v_deactivate = g2.aggregateMessages[(Int, Long)]( //(status, otherVertex)
         d => {
           if (d.dstAttr._1 == 0 && d.srcAttr._1 == 0){ //not selected vertices
               if ((d.srcId == d.dstAttr._2) && (d.dstAttr._4 == 1) && (d.srcAttr._4 == 0)) { //from src to dst
@@ -118,21 +85,31 @@ select a random neighbors
     }
 
     /* relabeled the edge that is selected
-    */
-    val v_in = g.vertices.filter({case (id, x) => (x._1 == 1)}).collect()
+    
+    val v_in = result.vertices.filter({case (id, x) => (x._1 == 1)}).collect()
     for (x <- v_in){
-      g = g.mapEdges(
+      result = result.mapEdges(
        id => if ((id.srcId == x._2._2 && id.dstId == x._1) || (id.dstId == x._2._2 && id.srcId == x._1)) (1) else (id.attr)
        )
     }
+	*/
 
     return g
   }
 
-//var result = Israli(g_in)
-
-
   def main(args: Array[String]) {
+	  /*
+    val conf = new SparkConf().setAppName("final_project")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+      val startTimeMillis = System.currentTimeMillis()
+      val edges = sc.textFile(args(0)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val g = Graph.fromEdges[(Int, Long), Int](edges, (0, 0L), edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
+
+*/
+
+
 
     val conf = new SparkConf().setAppName("final_project")
     val sc = new SparkContext(conf)
@@ -145,19 +122,29 @@ select a random neighbors
     }
 
       val startTimeMillis = System.currentTimeMillis()
-      val edges = sc.textFile(args(1)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
-      val g = Graph.fromEdges[Int, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
-
-      val g2 = Israli(g)
-
+      val edges = sc.textFile(args(0)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
+      val g = Graph.fromEdges[(Int, Long), Int](edges, (0, 0L), edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
+      
+      var g2 = Israli(g)
+	  
+      var v_in = g2.vertices.filter({case (id, x) => (x._1 == 1)}).collect()
+      for (x <- v_in){
+        g2 = g2.mapEdges(
+         id => if ((id.srcId == x._2._2 && id.dstId == x._1) || (id.dstId == x._2._2 && id.srcId == x._1)) (1) else (id.attr)
+         )
+      }
+	  
+	  
       val endTimeMillis = System.currentTimeMillis()
       val durationSeconds = (endTimeMillis - startTimeMillis) / 1000
       println("==================================")
       println("Israli's algorithm completed in " + durationSeconds + "s.")
       println("==================================")
+	  
       var g2df = spark.createDataFrame(g2.edges.filter({case (id) => (id.attr == 1)}))
       g2df = g2df.drop(g2df.columns.last)               
       g2df.coalesce(1).write.format("csv").mode("overwrite").save(args(2))
+	  
 
 
   }
