31e18409f47f29fec036b4ff3547266270758bc2
Jessica  Fong Ng <jessicafongng@Jessicas-MacBook-Air-2.local>
2021-05-09 18:44:03 -0400

    increase matching case

--- src/main/scala/final_project/main.scala
+++ src/main/scala/final_project/main.scala
@@ -16,11 +16,11 @@ object main{
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-  def LubyMIS(g_in: Graph[Float, Int]): Graph[(Float, Float), (Int, Float, Float)] = {
+  def LubyMIS(g_in: Graph[Double, Int]): Graph[(Double, Double), (Int, Double, Double)] = {
     var active_v = 2L
     var counter = 0
     val r = scala.util.Random
-    var g = g_in.mapEdges((i) => (-1, -1F, -1F)).mapVertices((id, i) => (-1F, -1F)) //[(float),(status, float)]
+    var g = g_in.mapEdges((i) => (-1, -1D, -1D)).mapVertices((id, i) => (-1D, -1D)) //[(float),(status, float)]
 	/*
 		active = -1
 		deactivate = 0
@@ -28,8 +28,8 @@ object main{
 	*/
     while (active_v >= 1) { // remaining edges
 	  counter += 1
-      g = g.mapEdges((i) => (i.attr._1, r.nextFloat, r.nextFloat)) //give active edges random number
-      var v_in = g.aggregateMessages[(Float, Float)]( 
+      g = g.mapEdges((i) => (i.attr._1, r.nextDouble, r.nextDouble)) //give active edges random number
+      var v_in = g.aggregateMessages[(Double, Double)]( 
         d => { // Map Function
 			if (d.attr._1 == 1 || d.attr._1 == 0) { //edge is already deactive
 				d.sendToDst(d.attr._1,d.attr._1); //mark deactive
@@ -48,9 +48,9 @@ object main{
 	  //produce new edges
 	  var n_edges = g2.triplets.map(
 		  t => {if ((t.attr._1) == 1 || (t.attr._1) == 0) {Edge(t.srcId, t.dstId,(t.attr._1, t.attr._2, t.attr._3));} //remain
-		  		else if ((t.srcAttr == 1D) || ( t.dstAttr == 1D)) {Edge(t.srcId, t.dstId,(0, t.attr._2, t.attr._3));} 
+		  		else if ((t.srcAttr._1 == 1) || ( t.dstAttr._1 == 1)) {Edge(t.srcId, t.dstId,(0, t.attr._2, t.attr._3));} 
 				else {// (0,0), (0, -1), (-1,-1)
-						if (t.srcAttr == t.dstAttr) (Edge(t.srcId, t.dstId,(1, t.attr._2, t.attr._3))) else (Edge(t.srcId, t.dstId,(t.attr._1, t.attr._2, t.attr._3)))
+						if ((t.srcAttr._1 == t.dstAttr._1) && (t.srcAttr._2 == t.dstAttr._2)) (Edge(t.srcId, t.dstId,(1, t.attr._2, t.attr._3))) else (Edge(t.srcId, t.dstId,(t.attr._1, t.attr._2, t.attr._3)))
 		  		}
 	  	  }
   		)
@@ -98,7 +98,7 @@ def augmenting(g_matching:Graph[(Float, Long), (Int, Float)]):Graph[(Float, Long
 
     val startTimeMillis = System.currentTimeMillis()
     val edges = sc.textFile(args(0)).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
-    val g = Graph.fromEdges[Float, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
+    val g = Graph.fromEdges[Double, Int](edges, 0D, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
     var g2 = LubyMIS(g)
 	val ans = g2.mapEdges((i) => i.attr._1)
 	
